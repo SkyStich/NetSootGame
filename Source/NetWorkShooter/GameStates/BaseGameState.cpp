7b710633a4908fd9b022593060ac42c79b5c81b4
Stich <SkyStich@users.noreply.github.com>
2021-01-17 04:10:11 +0300

    Added a function to increment round time by one second

--- Source/NetWorkShooter/GameStates/BaseGameState.cpp
+++ Source/NetWorkShooter/GameStates/BaseGameState.cpp
@@ -2,4 +2,22 @@
 
 
 #include "BaseGameState.h"
+#include "Net/UnrealNetwork.h"
 
+void ABaseGameState::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(ABaseGameState, PlayTime);
+}
+
+void ABaseGameState::IncrementPlayTime()
+{
+    FTimespan const TempSpan(0, 0, 1);
+    PlayTime += TempSpan;
+}
+
+void ABaseGameState::StartGameTimer()
+{
+    GetWorld()->GetTimerManager().SetTimer(TimeTickHandle, this, &ABaseGameState::IncrementPlayTime, 1, true);
+}
--- Source/NetWorkShooter/GameStates/BaseGameState.h
+++ Source/NetWorkShooter/GameStates/BaseGameState.h
@@ -13,5 +13,26 @@ UCLASS()
 class NETWORKSHOOTER_API ABaseGameState : public AGameState
 {
 	GENERATED_BODY()
-	
+
+    /** Add to PlayTime One Second*/
+    UFUNCTION()
+    void IncrementPlayTime();
+    
+public:
+
+    /** Start time for increment One second in PlayTime */
+    UFUNCTION()
+    void StartGameTimer();
+
+    virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+    
+protected:
+
+    /** Game duration */
+    UPROPERTY(Replicated, BlueprintReadOnly, Category = "GameState|PlayTime")
+    FTimespan PlayTime;
+
+private:
+
+    FTimerHandle TimeTickHandle;
 };
