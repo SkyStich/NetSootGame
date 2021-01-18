ba4c9160c224129e7a4f231c0768460014785978
Stich <SkyStich@users.noreply.github.com>
2021-01-18 03:38:57 +0300

    Create EveryManForHimselfGameState.cpp

---
+++ Source/NetWorkShooter/GameStates/EveryManForHimselfGameState.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EveryManForHimselfGameState.h"
+#include "Net/UnrealNetwork.h"
+
+AEveryManForHimselfGameState::AEveryManForHimselfGameState()
+{
+   
+}
+
+void AEveryManForHimselfGameState::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
